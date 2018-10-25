521fb0f28f93025b1edf4c1eb78c30659ad45c29
sarimk <43296144+sarimk@users.noreply.github.com>
2018-10-25 21:02:15 +0100

    created isWindy function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-//alert('Hello World');
+function isWindy(speed) {
+    if (speed > 5) {
+        alert('It is windy');
+    } else {
+        alert('It is not windy');
+    }
+}
+
+isWindy(10);
+isWindy(4);
